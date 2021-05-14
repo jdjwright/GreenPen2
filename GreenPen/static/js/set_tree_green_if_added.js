62fdca49ad91459dcd6d85e05cc04db0039015ad
James Wright <jdjwright@gmail.com>
2021-05-14 17:42:02 +0800

    Removed redundant debug statements

--- GreenPen/static/js/set_tree_green_if_added.js
+++ GreenPen/static/js/set_tree_green_if_added.js
@@ -2,27 +2,23 @@
 $('#id_syllabus-tree').bind('loaded.jstree', function(e, data) {
     $('.select2-hidden-accessible').each(function() {
         $(this).on('select2:select', function (e) {
-            console.log(e.params.data.id + " added");
             if(isNaN( points_added[e.params.data.id])) {
                 points_added[e.params.data.id] = 1
             }
             else {
                 points_added[e.params.data.id] = points_added[e.params.data.id] + 1;
             }
-            console.log(points_added)
             set_tree_inserted_syllabus_points()
 
         })
         $(this).on('select2:unselect', function (e) {
 
-            console.log(e.params.data.id + " removed");
             if(points_added[e.params.data.id] == 1) {
                 delete(points_added[e.params.data.id])
             }
             else {
                 points_added[e.params.data.id] = points_added[e.params.data.id] - 1;
             }
-            console.log(points_added)
             set_tree_inserted_syllabus_points()
 
         })
